9540811f34429fab3ee6c8a5cea3323eb93ab16a
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-22 14:15:04 +0200

    bc

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -13,13 +13,7 @@
         "base",
     ],
     "data": [
-        "security/ir.model.access.csv",
-        "views/estate_property_view.xml",
-        "views/estate_property_type_view.xml",
-        "views/estate_property_tag_view.xml",
-        # 'views/estate_property_offer_view.xml',
-        "views/estate_property_menu_view.xml",
-        "views/template.xml",
+   
     ],
     "installable": True,
     "application": True,
--- estate/controllers/main.py
+++
@@ -1,6 +0,0 @@
-from odoo import http
-
-class NewPage(http.Controller):
-    @http.route('/new-page/', auth='public', website=True)
-    def index(self, **kw):
-        return http.request.render('estate.index')
