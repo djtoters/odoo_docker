932b26c1087134696b75d432f5d47cf41cfaa1b3
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-23 16:13:18 +0200

    Drain

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = 'estate.property_estate'
-    _description = 'Property of estate module XDzefsfdjgfgfv' 
+    _description = 'Property of estate module XDzefdjgfgfv' 
     _rec_name = 'title'
     _order = 'sequence, id desc'
 
