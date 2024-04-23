db01feedfd8c32b82f215d6d89980501adb46ef9
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-23 08:23:23 +0200

    ezaaz

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -5,7 +5,7 @@ from odoo.exceptions import UserError
 
 class EstateProperty(models.Model):
     _name = 'estate.property_estate'
-    _description = 'Property of estate module XD:;DDDDD' 
+    _description = 'Property of estate module XD:;DDDD' 
     _rec_name = 'title'
     _order = 'sequence, id desc'
 
