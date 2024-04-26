ec02330ea07c1779d18409883c5228be2acc3b69
Maxime007XD <mdangelo2002pro@gmail.com>
2024-04-26 08:38:06 +0200

    rgthj

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    "name": "Estate",
+    "name": "Estanhjte",
     "version": "1.0",
     "category": "Sales/estate",
     "sequence": 1000,
